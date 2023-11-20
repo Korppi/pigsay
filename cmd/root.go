2850b40ede7120b7bef3eef09ea72a151a529998
Korppi <3602291+Korppi@users.noreply.github.com>
2023-11-20 23:19:40 +0200

    added proto of speech bubble that grows based on text input size

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"errors"
 	"os"
 	"strings"
 
@@ -13,9 +14,64 @@ import (
 
 var eyes string
 
+var pig string = `
+         _/|________
+        / %s         \
+       E,            |S
+        \___________/
+         WW       WW
+`
+
+var example string = `pigsay
+	Display help
+pigsay Hello World
+	Display pig saying "Hello World"
+pigsay -e @ Hello World
+	Display pig with custom eye @ saying "Hello World"
+pigsay --eyes @ Hello World
+	Display pig with custom eye @ saying "Hello World"
+`
+
+func buildSpeechBubble(text string) string {
+	wrapper := wordwrap.Wrapper(30, true)
+	wrapped := wrapper(text)
+	splitMessage := strings.Split(wrapped, "\n")
+	longest := 0
+	for _, v := range splitMessage {
+		l := len([]rune(v))
+		if l > longest {
+			longest = l
+		}
+	}
+	bubbleLeftPadding := 2
+	var bubbleSize int
+	if bubbleSize = 3; longest > bubbleSize {
+		bubbleSize = longest
+	}
+	//header
+	bubble := strings.Repeat(" ", bubbleLeftPadding+1) + strings.Repeat("_", bubbleSize+1) + "\n"
+	bubble += strings.Repeat(" ", bubbleLeftPadding) + "/" + strings.Repeat(" ", bubbleSize+1) + "\\\n"
+	//body
+	size := len(splitMessage)
+	for _, v := range splitMessage {
+		l := len([]rune(v))
+		if size == 1 && (l == 1 || l == 2) {
+			bubble += strings.Repeat(" ", bubbleLeftPadding-1) + "|" + strings.Repeat(" ", 2) + v + strings.Repeat(" ", 2) + "|"
+		} else {
+			bubble += strings.Repeat(" ", bubbleLeftPadding-1) + "|" + "  " + v + strings.Repeat(" ", longest-l+1) + "|"
+		}
+		bubble += "\n"
+	}
+	//footer
+	bubble += strings.Repeat(" ", bubbleLeftPadding) + "\\" + strings.Repeat("_", 1) + strings.Repeat(" ", 2) + strings.Repeat("_", bubbleSize-2) + "/\n"
+	bubble += strings.Repeat(" ", bubbleLeftPadding+2) + "\\|"
+
+	return bubble
+}
+
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "pigsay [flags] <MESSAGE>",
+	Use:   "pigsay [-e] <MESSAGE>",
 	Short: "Make a pig say things!",
 	Args: func(cmd *cobra.Command, args []string) error {
 		if len(args) == 0 {
@@ -24,30 +80,33 @@ var rootCmd = &cobra.Command{
 		}
 		return nil
 	},
-	Example: "  pigsay\n\tDisplay help\n  pigsay Hello World :)\n\tDisplay pig saying 'Hello World :)'\n  pigsay --eyes @ Now i have different eyes!\n\tDisplay pig with custom eyes",
+	Example: example,
 	Version: "1.0.0",
-	Run: func(cmd *cobra.Command, args []string) {
-		eye := eyes[0:1]
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len([]rune(eyes)) != 1 {
+			return errors.New("eyes should be only 1 character, no less no more")
+		}
+		bubble := buildSpeechBubble(strings.Join(args, " "))
+		/*
+			splitMessage := strings.Split(wrappedWithIndent, "\n")
+			longest := 0
+			finalMessage := ""
+			for _, v := range splitMessage {
+				l := len(v)
+				if l > longest {
+					longest = l
+				}
+				if finalMessage != "" {
+					finalMessage += "\n"
+				}
+				finalMessage += strings.Repeat(" ", 6) + v
+			}*/
 		wrapper := wordwrap.Wrapper(30, true)
 		wrapped := wrapper(strings.Join(args, " "))
-		wrappedWithIndent := wordwrap.Indent(wrapped, "  ", true)
-		splitMessage := strings.Split(wrappedWithIndent, "\n")
-		longest := 0
-		finalMessage := ""
-		for _, v := range splitMessage {
-			l := len(v)
-			if l > longest {
-				longest = l
-			}
-			if finalMessage != "" {
-				finalMessage += "\n"
-			}
-			finalMessage += strings.Repeat(" ", 6) + v
-		}
-
-		bubbleAndText := strings.Repeat(" ", 6) + strings.Repeat("-", longest+2) + "\n" + finalMessage + "\n" + strings.Repeat(" ", 6) + strings.Repeat("-", longest+2)
-		cmd.Printf(bubbleAndText)
-		cmd.Printf("\n          \\|\n\n            _/|________\n           / " + eye + "         \\\n          E,            |S\n           \\___________/\n            WW       WW")
+		cmd.Println(wrapped)
+		cmd.Printf(bubble)
+		cmd.Printf(pig, eyes)
+		return nil
 	},
 }
 
