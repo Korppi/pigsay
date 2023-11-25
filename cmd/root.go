1dc27714380bef61512484e8394524357c43dcad
Korppi <3602291+Korppi@users.noreply.github.com>
2023-11-25 14:49:08 +0200

    cleaned some comments

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2023 Teppo Kavander
 */
 package cmd
 
@@ -33,6 +33,7 @@ pigsay --eyes @ Hello World
 	Display pig with custom eye @ saying "Hello World"
 `
 
+// Builds speech bubble and text wrapped inside it
 func buildSpeechBubble(text string) string {
 	wrapper := wordwrap.Wrapper(30, true)
 	wrapped := wrapper(text)
@@ -102,13 +103,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pigsay.yaml)")
 	rootCmd.PersistentFlags().StringVarP(&eyes, "eyes", "e", "o", "give pig different eyes")
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
